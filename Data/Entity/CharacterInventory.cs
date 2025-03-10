e80e93b1b65474f204ecbf1549168eaf4a4533b2
not-oshi <not_oshi_mhm@proton.me>
2025-03-10 23:59:30 +0700

    test

---
+++ Data/Entity/CharacterInventory.cs
@@ -0,0 +1,11 @@
+﻿namespace tiefebot.Data.Entity;
+
+public class CharacterInventory
+{
+    public Guid Id { get; set; }
+    public Character Character { get; set; }
+    public Guid CharacterId { get; set; }
+    public ICollection<Item> Items { get; set; }
+    //public int Count { get; set; }
+    //public bool IsEquipped { get; set; } 
+}
